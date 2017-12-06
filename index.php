c2db5335ae79621b80c9898ff830cb02b3075bbd
MitchMan96 <31675855+MitchMan96@users.noreply.github.com>
2017-12-05 20:59:23 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-
+<?php
+$servername = "sql212.epizy.com";
+$username = "epiz_20708161";
+$password = "TFyNhU4q03";
+$dbname = "epiz_20708161_BluesStatistics";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "<h1>Connected successfully</h1>";
+$sql = "SELECT * FROM tasks";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<p>id: " . $row["id"]. " - Title: " . $row["title"]. " -Description: " . $row["description"]. "</p>";
+    }
+} else {
+    echo "<p>0 results</p>";
+}
+$conn->close();
+?>
