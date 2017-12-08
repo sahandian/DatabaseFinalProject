4a122493a681cb3b2037a3f97d2786db555718e1
sahandian <ssyxf@mail.missouri.edu>
2017-12-08 01:15:50 -0600

    Updated update.php to final changes

--- update.php
+++ update.php
@@ -1,30 +1,43 @@
 <?php
-if(isset($_POST['update'])){
-    $playerId = $_POST['playerId'];
-    $Name = $_POST['Name'];
-    $GP = $_POST['GP'];
-    $G = $_POST['G'];
-    $A = $_POST['A'];
-    $P = $_POST['P'];
-    $PM = $_POST['PM'];
-    $PIM = $_POST['PIM'];
-    $PPG = $_POST['PPG'];
-    $PPP = $_POST['PPP'];
-    $SHG = $_POST['SHG'];
-    $SHP = $_POST['SHP'];
-    $GWG = $_POST['GWG'];
-    $OTG = $_POST['OTG'];
-    $Shots = $_POST['Shots'];
-    
-    $sql = mysqli_query("UPDATE stats SET Name='$Name', GP='$GP',G='$G',A='$A',P='$P',PM='$PM',PIM='$PIM',PPG='$PPG',PPP='$PPP',SHG='$SHG',SHP='$SHP',GWG='$GWG',OTG='$OTG',Shots='$Shots' WHERE playerId='$playerId'");
-    
-    $retrieval = mysqli_query($sql, $conn);
-    if(! $retrieval){
-        die('Could not update data:' . mysqli_error());
-    }
-    echo "Updated data successfully\n";
-    
 
+$servername = "sql302.epizy.com";
+$username = "epiz_21190636";
+$password = "password";
+$dbname = "epiz_21190636_stats";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
+$id = "0";
+$name =  $_POST['name'];
+$team= $_POST['Team'];
+$GP =  $_POST['GP'];
+$G =  $_POST['G'];
+$A =  $_POST['A'];
+$P =  $_POST['PTS'];
+$plus =  $_POST['PM'];
+$PIM =  $_POST['PIM'];
+$PPG =  $_POST['PPG'];
+$PPP =  $_POST['PPP'];
+$SHG =  $_POST['SHG'];
+$SHP =  $_POST['SHP'];
+$GWG =  $_POST['GWG'];
+$OTG =  $_POST['OTG'];
+$Shots =  $_POST['Shots'];
+
+$sql = "UPDATE stats SET Name='$name',Team='$team',GP='$GP',G='$G',A='$A',P='$P',PM='$plus',PIM='$PIM',PPG='$PPG',PPP='$PPP',SHG='$SHG',SHP='$SHP',GWG='$GWG',OTG='$OTG',Shots='$Shots' WHERE Name= '$name'";
+
+if ($conn->query($sql) === TRUE) {
+    echo "SUCCESS";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
+$conn->close();
+
+header("refresh:2; url=index.php");
+
 ?>
