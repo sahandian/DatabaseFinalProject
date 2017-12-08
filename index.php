380d741dfa513effa00d96522e25709fe4ff503a
sahandian <ssyxf@mail.missouri.edu>
2017-12-07 21:48:54 -0600

    This is the full index.php file

--- index.php
+++ index.php
@@ -1,30 +1,331 @@
-<?php
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width = device-width, initial-scale = 1"> 
+    <title>CS3380</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+<style>
+
+    .jumbotron{
+        background-color: dodgerblue;
+        color: white;
+    }
+    
+    .tab-content{
+        border-left: 1px solid #ddd;
+        border-right: 1px solid #ddd;
+        border-bottom: 1px solid #ddd;
+        padding: 10px;
+    }
+    
+    .nav-tabs {
+        margin-bottom: 0;
+    }
+    
+    .img-fluid{
+        width: 100%;
+    }
+    
+    .header{
+        font-style: bold;
+    }
+    
+    .borderTime{
+        border-top: 10px solid dodgerblue;
+        border-left: 10px solid dodgerblue;
+        border-right: 10px solid red;
+        border-bottom: 10px solid red;
+        border-radius: 10px;
+        
+    }
+</style>
+    
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="page-header text-center">
+        <h1 class="header">Blues vs Hawks Statistics Page</h1>
+        </div>
+        <!--<img src="http://covermyfb.com/media/covers/thumb/8412-saint-louis-blues.jpg" class="img-fluid" alt="Responsive Image">-->
+        <div class="borderTime">
+        <img src="https://orig00.deviantart.net/b907/f/2014/109/d/c/2014_nhl_playoffs_rd_1_blues_vs__blackhawks_by_epoole88-d7f2zx3.jpg" class="img-fluid borderTime">
+        </div>
+        <div class="jumbotron">
+        
+            <h2 class="text-center">Welcome to our Site Developed for CS3380</h2>
+            <h4 class="text-center">Developers:</h4>
+            <h5 class="text-center" style="color: black;">Sahand Sadri, Daniel Weiss, Kevin Hunkins, Zachary Pierucci</h5>
+            <h5 class="text-center" style="color: black;">Mitchell Hoffmann, Daniel Clubb, Quinn Rydland, Hayden Harrill</h5>
+            
+        <!--<p> HOyl asdjkfakb a dfiakshdf asdfham,  adskfna  asdkfna d aksdbfka dskf akjsdbnfasdlmf a,sd fkjabsdkf lamsd flkansdlkf lmasd flasdfln</p>
+        
+        <p>
+            <a href="#" class="btn btn-default btn-lg" role="button">More Info</a>
+            <button type="submit" class="btn btn-danger" role="button">Button</button>
+            <input type="button" value="Info" class="btn btn-info">
+            
+            <button type="submit" class="btn-primary btn-sm">Primary</button>
+            <button type="submit" class="btn btn-success btn-xs">Success</button>
+            <button type="submit" class="btn btn-warning btn-lg">Warning</button>
+            <button type="submit" class="btn btn-link btn-lg">Link</button>
+            
+            <button type="button" class="btn btn-lg btn-primary" disabled="disabled">Primary</button>
+            <a href="#" class="btn btn-default btn-lg disabled" role="button">Link</a>
+            
+            <div class="btn-group btn-group-lg" role="group">
+                <button type="button" class="btn btn-default">Small</button>
+                <button type="button" class="btn btn-default">Medium</button>
+                <button type="button" class="btn btn-default">Large</button>
+            </div>
+            
+        </p> -->
+        </div>
+        
+        <table class="table table-bordered table-striped table-hover">
+            <thead>
+                <tr>
+                <td class="text-center" colspan="14">Team Statistics</td>
+                </tr>
+            </thead>
+        
+            <tbody>
+                <tr>
+                    <th>Team</th>
+                    <th>City</th>
+                    <th>TeamCode</th>
+                    <th>Games Played</th>
+                    <th>Points</th>
+                    <th>Wins</th>
+                    <th>Losses</th>
+                    <th>OTL</th>
+                    <th>Division</th>
+                    <th>Conferences</th>
+                    <th>Stanley Cups</th>
+                
+                </tr>
+            <?php
 
 
 
 
-$servername = "bluesstatistics3380.cvkr0vitka5o.us-east-1.rds.amazonaws.com:3306";
-$username = "cs3380";
-$password = "blues3380";
-$dbname = "BluesStatistics3380";
+$servername = "sql302.epizy.com";
+$username = "epiz_21190636";
+$password = "password";
+$dbname = "epiz_21190636_stats";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "<h1>Connected successfully</h1>";
-$sql = "SELECT * FROM St.louis blues";
+#echo "<h1>Connected successfully</h1>";
+$sql = "SELECT * FROM teams";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<p>id: " . $row["Name"] . "</p>";
+        echo "<tr>";
+echo "<td>" . $row['Team'] . "</td>" ;
+echo "<td>" . $row['City'] . "</td>" ;
+echo "<td>" . $row['TeamCode'] . "</td>" ;
+echo "<td>" . $row['GamesPlayed'] . "</td>" ;
+echo "<td>" . $row['Points'] . "</td>" ;
+echo "<td>" . $row['Wins'] . "</td>" ;
+echo "<td>" . $row['Losses'] . "</td>" ;
+echo "<td>" . $row['OvertimeLosses'] . "</td>" ;
+echo "<td>" . $row['Division'] . "</td>" ;
+echo "<td>" . $row['Conferences'] . "</td>" ;        
+echo "<td>" . $row['StanleyCups'] . "</td>" ;
+echo "</tr>";
+
     }
 } else {
     echo "<p>0 results</p>";
 } //test
 $conn->close();
 ?>
- 
+                
+            </tbody>
+        </table>
+        
+        <table class="table table-bordered table-striped table-hover">
+            <thead>
+            <tr>
+            <td class="text-center" colspan="14">Blues Statistics</td>
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                
+                    <th>Player</th>
+                    <th>GP</th>
+                    <th>G</th>
+                    <th>A</th>
+                    <th>PTS</th>
+                    <th>+/-</th>
+                    <th>PIM</th>
+                    <th>PPG</th>
+                    <th>PPP</th>
+                    <th>SHG</th>
+                    <th>SHP</th>
+                    <th>GWG</th>
+                    <th>OTG</th>
+                    <th>SHOTS</th>
+                </tr>
+                
+<?php
+
+
+
+
+$servername = "sql302.epizy.com";
+$username = "epiz_21190636";
+$password = "password";
+$dbname = "epiz_21190636_stats";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+#echo "<h1>Connected successfully</h1>";
+$sql = "SELECT * FROM stats WHERE Team = 'STL'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>";
+echo "<td>" . $row['Name'] . "</td>" ;
+echo "<td>" . $row['GP'] . "</td>" ;
+echo "<td>" . $row['G'] . "</td>" ;
+echo "<td>" . $row['A'] . "</td>" ;
+echo "<td>" . $row['P'] . "</td>" ;
+echo "<td>" . $row['+/-'] . "</td>" ;
+echo "<td>" . $row['PIM'] . "</td>" ;
+echo "<td>" . $row['PPG'] . "</td>" ;
+echo "<td>" . $row['PPP'] . "</td>" ;
+echo "<td>" . $row['SHG'] . "</td>" ;        
+echo "<td>" . $row['SHP'] . "</td>" ;
+echo "<td>" . $row['GWG'] . "</td>" ;
+echo "<td>" . $row['OTG'] . "</td>" ;
+echo "<td>" . $row['Shots'] . "</td>" ;
+echo "</tr>";
+
+    }
+} else {
+    echo "<p>0 results</p>";
+} //test
+$conn->close();
+?>
+                
+                
+                
+            
+            </tbody>
+        
+        
+        
+        
+        </table>
+        
+        
+                <table class="table table-bordered table-striped table-hover">
+            <thead>
+            <tr>
+            <td class="text-center" colspan="14">Hawks Statistics</td>
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                
+                    <th>Player</th>
+                    <th>GP</th>
+                    <th>G</th>
+                    <th>A</th>
+                    <th>PTS</th>
+                    <th>+/-</th>
+                    <th>PIM</th>
+                    <th>PPG</th>
+                    <th>PPP</th>
+                    <th>SHG</th>
+                    <th>SHP</th>
+                    <th>GWG</th>
+                    <th>OTG</th>
+                    <th>SHOTS</th>
+                </tr>
+                
+<?php
+
+
+
+
+$servername = "sql302.epizy.com";
+$username = "epiz_21190636";
+$password = "password";
+$dbname = "epiz_21190636_stats";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+#echo "<h1>Connected successfully</h1>";
+$sql = "SELECT * FROM stats WHERE Team = 'CHI'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>";
+echo "<td>" . $row['Name'] . "</td>" ;
+echo "<td>" . $row['GP'] . "</td>" ;
+echo "<td>" . $row['G'] . "</td>" ;
+echo "<td>" . $row['A'] . "</td>" ;
+echo "<td>" . $row['P'] . "</td>" ;
+echo "<td>" . $row['+/-'] . "</td>" ;
+echo "<td>" . $row['PIM'] . "</td>" ;
+echo "<td>" . $row['PPG'] . "</td>" ;
+echo "<td>" . $row['PPP'] . "</td>" ;
+echo "<td>" . $row['SHG'] . "</td>" ;        
+echo "<td>" . $row['SHP'] . "</td>" ;
+echo "<td>" . $row['GWG'] . "</td>" ;
+echo "<td>" . $row['OTG'] . "</td>" ;
+echo "<td>" . $row['Shots'] . "</td>" ;
+echo "</tr>";
+
+    }
+} else {
+    echo "<p>0 results</p>";
+} //test
+$conn->close();
+?>
+                
+                
+                
+            
+            </tbody>
+        
+        
+        
+        
+        </table>
+    </div>
+    
+    
+    
+    
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+
+</body>
+
+
+</html>
